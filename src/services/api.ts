e16ff9273e30956ecfb797af890f6d03e74cc916
Jessica Medeiros <jessicamedeirosp96@gmail.com>
2024-04-01 17:10:35 -0300

    url backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://puce-alert-cygnet.cyclic.app",
-  timeout: 5000,
+  baseURL: "https://my-json-server.typicode.com/jessicamedeirosp/cubos-burger/",
+  timeout: 1000,
   headers: {
     "Content-Type": "application/json",
   },
